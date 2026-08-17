8eaa7724f0b8275b40db465593c74a544ac2e934
Fateme Mirjalili <fatimamirjalili2000@gmail.com>
2026-08-17 18:23:08 +0330

    Cors error for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/fatememirjalili2000/NFT_dashboard", // <--- نام دقیق ریپوزیتوری خودت را بین دو اسلش بگذار
+  base: "/NFT_dashboard/",
 });
