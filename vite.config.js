83db61ca573435c100099212094c0f3f66f8789d
Fateme Mirjalili <fatimamirjalili2000@gmail.com>
2026-08-17 20:17:43 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/NFT_dashboard/", // <--- این خط حتماً همین‌طور باشد
+  base: "/NFT_dashboard/", // <--- نام دقیق ریپازیتوری همراه با اسلش در اول و آخر
 });
